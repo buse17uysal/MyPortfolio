09fa39ba8c14f7f7b4a5d67a2f1713e3b57f038b
Buse Uysal <buseuysal3158@gmail.com>
2024-11-26 17:29:24 +0300

    Create

---
+++ DAL/Entites/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace MyPortfolio.DAL.Entites
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
